0fde95cfee982de8c2986c09768cb10bb3960880
nikhilishere30 <nikhilishere30@gmail.com>
2021-10-28 21:58:06 +0530

    UserContext functionality

---
+++ client/src/components/UserContext.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+// createContext takes the value which is provided when there is no context provider present.
+export const UserContext = createContext(null);
