7511883df591ca48199edbde3d9782960622c1e1
Harsh-Modi278 <harshmodi278@gmail.com>
2021-12-09 14:14:13 +0530

    feat: Post controllers

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -4,7 +4,7 @@ const express = require("express");
 // mongoose related
 const mongoose = require("mongoose");
 const User = require("../models/User.js");
-const Post = require("../models/Post.js");
+const Article = require("../models/Article.js");
 const Comment = require("../models/Comment.js");
 
 // jwt related
@@ -54,5 +54,76 @@ const postsComment_post = (req, res, next) => {
     })
     .catch((err) => console.error(err));
 };
+
+const saveArticle = (obj) => {
+  new Article(obj).save((err, article) => {
+      if (err)
+          res.send(err)
+      else if (!article)
+          res.send(400)
+      else {
+          return article.addAuthor(req.body.author_id).then((_article) => {
+              return res.send(_article)
+          })
+      }
+      next();
+  })
+}
+
+const addArticle = (req, res, next) => {
+  let { text, title, claps, description } = req.body
+  if (req.files.image) {
+      cloudinary.uploader.upload(req.files.image.path, (result) => {
+          let obj = { text, title, claps, description, feature_img: result.url != null ? result.url : '' }
+          saveArticle(obj)
+          
+      },{
+          resource_type: 'image',
+          eager: [
+              {effect: 'sepia'}
+          ]
+      })
+  }else {
+      saveArticle({ text, title, claps, description, feature_img: '' })
+  }
+
+const getAll = (req, res, next) => {
+  Article.find(req.params.id)
+  .populate('author')
+  .populate('comments.author').exec((err, article)=> {
+      if (err)
+          res.send(err)
+      else if (!article)
+          res.send(404)
+      else
+          res.send(article)
+      next();            
+  })
+}
+
+const commentArticle = (req, res, next) => {
+  Article.findById(req.body.article_id).then((article)=> {
+      return article.comment({
+          author: req.body.author_id,
+          text: req.body.comment
+      }).then(() => {
+          return res.json({msg: "Done"})
+      })
+  }).catch(next);
+}
+
+const getArticle = (req, res, next) => {
+  Article.findById(req.params.id)
+  .populate('author')
+  .populate('comments.author').exec((err, article)=> {
+      if (err)
+          res.send(err)
+      else if (!article)
+          res.send(404)
+      else
+          res.send(article)
+      next()            
+  })
+}
 
 module.exports = { postsIndex_get, postId_get, postsComment_post };
